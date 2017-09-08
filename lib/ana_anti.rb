a053edd5000757bcedf2af8340f60851e7496b5b
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-09-08 16:06:59 -0700

    add anagramtesting function using .sort to alphabeticize strings

---
+++ lib/ana_anti.rb
@@ -0,0 +1,27 @@
+# Class Words
+#   def initialize(word1, word2)
+#     @word1 = word1
+#     @word2 = word2
+#   end
+#
+#   # def anagrams(word1, word2)
+#   #   return nil unless word1.size === word2.size
+#   #   lower_word1 = word1.downcase
+#   #   lower_word2 = word2.downcase
+#   #
+#   #   lower_word1.split.each_char do
+#   #     lower_word2.split.each_char do
+#   #
+#   #     end
+#   #   end
+#   #
+#   # end
+#
+#
+# end
+
+def anagrams(word1, word2)
+  word1.chars.sort.join === word2.chars.sort.join
+end
+
+puts "Anagram status: #{anagrams("hello", "olleh")}"
